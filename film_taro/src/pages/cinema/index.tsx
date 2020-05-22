e0be73f5aadc9a45424f6bfcc91f508aab239620
vanyol <912381732>
2020-05-22 18:04:33 +0800

    cinema page static

--- film_taro/src/pages/cinema/index.tsx
+++ film_taro/src/pages/cinema/index.tsx
@@ -1,11 +1,20 @@
-import { View } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import Footer from "../../components/Footer"
 import './index.styl'
 export default function Index() {
 
   return (
-    <View>
-      cinema
+    <View className="cinema">
+      <View className="header">header</View>
+      <View className="cinema-item">
+        <Text className="name">name</Text>
+        <Text className="detail">name</Text>
+        <View className="tags">
+          <Text className="tag">tag1</Text>
+          <Text className="tag">tag2</Text>
+          <Text className="tag">tag3</Text>
+        </View>
+      </View>
       <Footer />
     </View>
   )
