75f19360625e0add1b5bfe043db7d82f3868d8ff
vanyol <912381732>
2020-05-22 17:23:50 +0800

    mine page static

--- film_taro/src/pages/mine/index.tsx
+++ film_taro/src/pages/mine/index.tsx
@@ -1,10 +1,21 @@
-import { View } from "@tarojs/components"
+import { View, Text } from "@tarojs/components"
 import Footer from "../../components/Footer"
+import { AtAvatar, AtListItem } from 'taro-ui'
+import "taro-ui/dist/style/components/avatar.scss"
+import "taro-ui/dist/style/components/list.scss"
+import './index.styl'
+
 export default function Index() {
 
   return (
     <View>
-      mine
+      <View id="avatar-box">
+        <AtAvatar circle image='https://jdc.jd.com/img/200'></AtAvatar>
+        <Text className="login">登录/注册</Text>
+      </View>
+      <AtListItem title='我的订单' arrow='right' />
+      <AtListItem title='想看的电影' arrow='right' />
+      <AtListItem title='看过的电影' arrow='right' />
       <Footer />
     </View>
   )
