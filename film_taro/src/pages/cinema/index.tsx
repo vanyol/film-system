3a251df1386ce179c8942552a89bd13e9f2afde4
vanyol <912381732@qq.com>
2020-05-23 17:01:26 +0800

    cinema page get data from backend

--- film_taro/src/pages/cinema/index.tsx
+++ film_taro/src/pages/cinema/index.tsx
@@ -1,20 +1,39 @@
 import { View, Text } from '@tarojs/components'
 import Footer from "../../components/Footer"
 import './index.styl'
-export default function Index() {
+import ajax from '../../api/ajax'
+import { useState, useEffect } from '@tarojs/taro'
 
+function getCinemas() {
+  return ajax.get('getCinemaList')
+}
+
+interface Cinema {
+  cinema_name: string,
+  specified_address: string,
+  cinema_id: number
+}
+
+export default function Index() {
+  const [cinemaList, setCinemaList] = useState<Cinema[]>([])
+  useEffect(() => {
+    getCinemas().then(({ data }) => setCinemaList(data.data))
+  }, [setCinemaList])
   return (
     <View className="cinema">
       <View className="header">header</View>
-      <View className="cinema-item">
-        <Text className="name">name</Text>
-        <Text className="detail">name</Text>
-        <View className="tags">
-          <Text className="tag">tag1</Text>
-          <Text className="tag">tag2</Text>
-          <Text className="tag">tag3</Text>
+      {cinemaList.map(cinema => (
+        <View className="cinema-item">
+          <Text className="name">{cinema.cinema_name}</Text>
+          <Text className="detail">{cinema.specified_address}</Text>
+          <View className="tags">
+            <Text className="tag">小吃</Text>
+            <Text className="tag">4D厅</Text>
+            <Text className="tag">巨幕厅</Text>
+            <Text className="tag">杜比全景声厅</Text>
+          </View>
         </View>
-      </View>
+      ))}
       <Footer />
     </View>
   )
